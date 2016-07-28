d0e931f1ddff6c922f727eed8bfacf7f482d9ea6
Arnaud <onra@arnauds-macbook-pro.home>
2016-07-28 23:24:00 +0200

    Update colorWithHex function to color(hex: xxx).

--- flatuicolors.swift
+++ flatuicolors.swift
@@ -10,43 +10,43 @@
 import UIKit
 
 extension UIColor {
-
-    class func colorWithHex(hex: Int, alpha: CGFloat = 1.0) -> UIColor {
+    
+    class func color(hex: Int, alpha: CGFloat = 1.0) -> UIColor {
         let r = CGFloat((hex & 0xff0000) >> 16) / 255.0
         let g = CGFloat((hex & 0x00ff00) >>  8) / 255.0
         let b = CGFloat((hex & 0x0000ff) >>  0) / 255.0
         return UIColor(red: r, green: g, blue: b, alpha: alpha)
     }
-
+    
     // green sea
-    class func turquoise()    -> UIColor { return UIColor.colorWithHex(0x1abc9c) }
-    class func greenSea()     -> UIColor { return UIColor.colorWithHex(0x16a085) }
+    class func turquoise()    -> UIColor { return UIColor.color(hex: 0x1abc9c) }
+    class func greenSea()     -> UIColor { return UIColor.color(hex: 0x16a085) }
     // green
-    class func emerald()      -> UIColor { return UIColor.colorWithHex(0x2ecc71) }
-    class func nephritis()    -> UIColor { return UIColor.colorWithHex(0x27ae60) }
+    class func emerald()      -> UIColor { return UIColor.color(hex: 0x2ecc71) }
+    class func nephritis()    -> UIColor { return UIColor.color(hex: 0x27ae60) }
     // blue
-    class func peterRiver()   -> UIColor { return UIColor.colorWithHex(0x3498db) }
-    class func belizeHole()   -> UIColor { return UIColor.colorWithHex(0x2980b9) }
+    class func peterRiver()   -> UIColor { return UIColor.color(hex: 0x3498db) }
+    class func belizeHole()   -> UIColor { return UIColor.color(hex: 0x2980b9) }
     // purple
-    class func amethyst()     -> UIColor { return UIColor.colorWithHex(0x9b59b6) }
-    class func wisteria()     -> UIColor { return UIColor.colorWithHex(0x8e44ad) }
+    class func amethyst()     -> UIColor { return UIColor.color(hex: 0x9b59b6) }
+    class func wisteria()     -> UIColor { return UIColor.color(hex: 0x8e44ad) }
     // dark blue
-    class func wetAsphalt()   -> UIColor { return UIColor.colorWithHex(0x34495e) }
-    class func midnightBlue() -> UIColor { return UIColor.colorWithHex(0x2c3e50) }
+    class func wetAsphalt()   -> UIColor { return UIColor.color(hex: 0x34495e) }
+    class func midnightBlue() -> UIColor { return UIColor.color(hex: 0x2c3e50) }
     // yellow
-    class func sunFlower()    -> UIColor { return UIColor.colorWithHex(0xf1c40f) }
-    class func flatOrange()   -> UIColor { return UIColor.colorWithHex(0xf39c12) }
+    class func sunFlower()    -> UIColor { return UIColor.color(hex: 0xf1c40f) }
+    class func flatOrange()   -> UIColor { return UIColor.color(hex: 0xf39c12) }
     // orange
-    class func carrot()       -> UIColor { return UIColor.colorWithHex(0xe67e22) }
-    class func pumkin()       -> UIColor { return UIColor.colorWithHex(0xd35400) }
+    class func carrot()       -> UIColor { return UIColor.color(hex: 0xe67e22) }
+    class func pumkin()       -> UIColor { return UIColor.color(hex: 0xd35400) }
     // red
-    class func alizarin()     -> UIColor { return UIColor.colorWithHex(0xe74c3c) }
-    class func pomegranate()  -> UIColor { return UIColor.colorWithHex(0xc0392b) }
+    class func alizarin()     -> UIColor { return UIColor.color(hex: 0xe74c3c) }
+    class func pomegranate()  -> UIColor { return UIColor.color(hex: 0xc0392b) }
     // white
-    class func clouds()       -> UIColor { return UIColor.colorWithHex(0xecf0f1) }
-    class func silver()       -> UIColor { return UIColor.colorWithHex(0xbdc3c7) }
+    class func clouds()       -> UIColor { return UIColor.color(hex: 0xecf0f1) }
+    class func silver()       -> UIColor { return UIColor.color(hex: 0xbdc3c7) }
     // gray
-    class func asbestos()     -> UIColor { return UIColor.colorWithHex(0x7f8c8d) }
-    class func concerte()     -> UIColor { return UIColor.colorWithHex(0x95a5a6) }
-
+    class func asbestos()     -> UIColor { return UIColor.color(hex: 0x7f8c8d) }
+    class func concerte()     -> UIColor { return UIColor.color(hex: 0x95a5a6) }
+    
 }
